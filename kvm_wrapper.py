46264f965f43296569b1bca7d9c771ffb537fb44
Stefano  Pirrello <spirrello@liaison.com>
2017-01-03 20:56:29 -0500

    updated kvm_wrapper.py

--- kvm_wrapper.py
+++ kvm_wrapper.py
@@ -60,20 +60,7 @@ def get_password():
 
 
 def main():
-   destroy_parser = argparse.ArgumentParser(
-       description='Process args for destorying KVM guests.')
    
-
-   destroy_parser.add_argument('-de', '--destroy', required=False, action='store',
-                       help='Destroy VMs.  Include the name of VM to destory.')
-   destroy_args = destroy_parser.parse_args()
-   if destroy_args:
-        os.system("virsh start " + destroy_args)
-        os.system("virsh destroy " + destroy_args)
-        os.system("virsh undefine " + destroy_args)
-        print("Deleting the VM image....")
-        os.system("rm -f /var/lib/libvirt/images/" + destroy_args + ".img")
-        sys.exit()
 
    parser = argparse.ArgumentParser(
        description='Process args for building KVM guests.')
